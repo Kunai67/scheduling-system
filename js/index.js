16ec5f5f185e904c0f4bd25513e976bb4dc03382
Ming-desu <markrecovery23@gmail.com>
2019-10-25 18:27:06 +0800

    Home landing page design

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var header = document.querySelector('.nav-header');
+var nav = document.querySelector('.nav-content');
+
+window.addEventListener('scroll', () => {
+    let scrollY = window.scrollY;
+
+    if (scrollY > header.clientHeight) {
+        header.classList.add('animate');
+        nav.style.color = "#fef";
+    }
+    else {
+        header.classList.remove('animate');
+        nav.style.color = "#333";
+    }
+});
